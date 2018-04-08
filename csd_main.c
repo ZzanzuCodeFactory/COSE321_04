78e70374beae371cc1f772c75320935cd1fd439b
zzanzu <lees3301@gmail.com>
2018-04-08 22:06:51 +0900

    edited for submtting

--- csd_main.c
+++ csd_main.c
@@ -1,38 +1,13 @@
 #include<stdio.h>
 
 void csd_main() {
-//	unsigned char *sw;
 	unsigned char *led;
 	unsigned char *input_char;
-	unsigned char *input_saving;
 	int iteration_num;
 	int i;
 
 	led = (unsigned char *) 0x41200000;
-	input_char = (unsigned char *) 0x00001000;
-	input_saving = (unsigned char *) 0x2000;
-//	sw = (unsigned *) 0x41210000;
-	iteration_num = (int *) 0x1000000; // 임의 주소
-
-//	if(*sw == 0x00) {
-//		iteration_num = 350000000;
-//	} else if(*sw == 0x01) {
-//		iteration_num = 280000000;
-//	} else if(*sw >= 0x02 && *sw < 0x04) {
-//		iteration_num = 245000000;
-//	} else if(*sw >= 0x4 && *sw < 0x08) {
-//		iteration_num = 210000000;
-//	} else if(*sw >= 0x08 && *sw < 0x10) {
-//		iteration_num = 175000000;
-//	} else if(*sw >= 0x10 && *sw < 0x20) {
-//		iteration_num = 140000000;
-//	} else if(*sw >= 0x20 && *sw < 0x40) {
-//		iteration_num = 105000000;
-//	} else if(*sw >= 0x40 && *sw < 0x80) {
-//		iteration_num = 70000000;
-//	} else if(*sw >= 0x80) {
-//		iteration_num = 35000000;
-//	}
+	input_char = (unsigned char *) 0x1000;
 
 	if(*input_char == 49) { // '1'
 		iteration_num = 8750000;
@@ -77,6 +52,5 @@ void csd_main() {
 
 	*led = 0x80;
 	for(i = 0 ; i < iteration_num ; i++);
-
 
 }
